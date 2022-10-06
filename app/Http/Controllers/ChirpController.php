37fe2ffc173106dee891de1b42dc9e0d739d2011
Colton Bonner <cbonner26118@gmail.com>
2022-10-06 16:13:22 -0600

    Create chirp funcionality and store in sqlite table.

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ChirpController extends Controller
 {
@@ -15,6 +16,9 @@ class ChirpController extends Controller
     public function index()
     {
         //
+        return Inertia::render('Chirps/index', [
+            //
+        ]);
     }
 
     /**
@@ -35,7 +39,13 @@ class ChirpController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
