895715de57d2b206ec793eed427bcde62933a2f0
Colton Bonner <cbonner26118@gmail.com>
2022-10-06 18:00:08 -0600

    Chirp component, relationship between user and chirp, populate dashboard with chirps, and added relative time format.

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -17,7 +17,7 @@ class ChirpController extends Controller
     {
         //
         return Inertia::render('Chirps/index', [
-            //
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),  //research pagination!!!
         ]);
     }
 
